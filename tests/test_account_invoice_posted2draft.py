50ce4d563e2846e8cdb359115320df3b57c58a8e
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-08-12 12:11:11 +0200

    Add Doctest ExceptionChecker to validate scenarios on python3

--- tests/test_account_invoice_posted2draft.py
+++ tests/test_account_invoice_posted2draft.py
@@ -1,12 +1,23 @@
 # This file is part of the account_invoice_posted2draft module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+import sys
+import re
 import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-from trytond.tests.test_tryton import doctest_checker
+from trytond.tests.test_tryton import Py23DocChecker
+
+
+class ExceptionChecker(Py23DocChecker):
+    def check_output(self, want, got, optionflags):
+        if sys.version_info[0] > 2:
+            got = re.sub("trytond.exceptions.(.*?)", "\\1", got)
+        return super(ExceptionChecker, self).check_output(
+            want, got, optionflags)
+doctest_checker = ExceptionChecker()
 
 
 class AccountInvoicePosted2draftTestCase(ModuleTestCase):
