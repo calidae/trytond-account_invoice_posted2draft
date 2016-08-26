faacd744fc8088da1b8ed85a4f0df08a8c7fb86f
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-08-26 11:34:47 +0200

    Use install_modules tools in scenario
    - This speedups scenario testing when run multiple times. Full details can
    be found on https://bugs.tryton.org/issue5794

--- tests/test_account_invoice_posted2draft.py
+++ tests/test_account_invoice_posted2draft.py
@@ -7,7 +7,7 @@ import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import Py23DocChecker
 
 
@@ -29,7 +29,7 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoicePosted2draftTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_invoice_posted2draft.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
